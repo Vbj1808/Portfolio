086c03083d229bf52235ffbeaffad6d8b616d889
BalajiJegadeesh <ijalab1808@gmail.com>
2025-01-07 00:30:52 -0500

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './sections/navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <h1>I am GOAT and the GOAT is procrastinating at this moment.</h1>
-    </>
+    <main className='max-w-7xl mx-auto'>
+      <Navbar/>
+    </main>
   )
 }
 
